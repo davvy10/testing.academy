c72dc985aa1ca828935cece03a5fb23cfdf71f01
David <david@localhost.localdomain>
2018-01-08 22:32:54 +0100

    zipper vecer pokus

--- bin/zipper.sh
+++ bin/zipper.sh
@@ -1,7 +1,11 @@
 #!bin/bash
 
-if [[ 
+FILE=$FILE
 
+if [[ $FILE rar.exe 1 $FILE.zip ]]; then
+        echo "$FILE\.zip is not RAR archive"
+        exit 1
+    fi
 
 if [[ $# gt -1 ]]; then
         echo "chyba" 1>&2
